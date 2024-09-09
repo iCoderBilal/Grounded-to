b2d0a250040e9f4c91efcb2c38e6e4dab5aed36f
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-09-09 23:13:39 +0500

    Navigation Added

--- EarthCEO/src/pages/homepage/Components/Header.jsx
+++ EarthCEO/src/pages/homepage/Components/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
                     href="#"
                     className=" font-Poppins block py-2 pr-4 pl-3 text-gray-300 hover:text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    Company
+                    Explore Land
                   </a>
                 </li>
                 <li className="bg-black hover:bg-[#1d1d1d] px-4 py-2 rounded-[30px] transition ease-in-out duration-300">
@@ -89,7 +89,7 @@ const Header = () => {
                     href="#"
                     className="font-Poppins block py-2 pr-4 pl-3 text-gray-300 hover:text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    Marketplace
+                    About USDL
                   </a>
                 </li>
                 <li className="bg-black hover:bg-[#1d1d1d] px-4 py-2 rounded-[30px] transition ease-in-out duration-300">
@@ -97,17 +97,10 @@ const Header = () => {
                     href="#"
                     className="font-Poppins block py-2 pr-4 pl-3 text-gray-300 hover:text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
-                    Features
+                    How It Works
                   </a>
                 </li>
-                <li className="bg-black hover:bg-[#1d1d1d] px-4 py-2 rounded-[30px] transition ease-in-out duration-300">
-                  <a
-                    href="#"
-                    className="font-Poppins block py-2 pr-4 pl-3 text-gray-300 hover:text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Team
-                  </a>
-                </li>
+
                 <li className="bg-black hover:bg-[#1d1d1d] px-4 py-2 rounded-[30px] transition ease-in-out duration-300">
                   <a
                     href="#"
